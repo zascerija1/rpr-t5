7bd251d65f0806aa84e6f82361d7770e1643272b
zascerija1 <zascerija1.etf.unsa.ba>
2018-11-13 18:39:53 +0100

    Final Calculator

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -18,7 +18,7 @@ public class Controller {
         this.displej.set(displej);
     }
     public Controller(){
-        displej=new SimpleStringProperty("");
+        displej=new SimpleStringProperty("0");
     }
 
     private SimpleStringProperty displej;
@@ -29,60 +29,126 @@ public class Controller {
 
 
     public void btn55(ActionEvent actionEvent) {
-        if(displej.get()=="0") displej.set("");
+        if(displej.get()=="0" || operacija.get()=="=") displej.set("");
         displej.set(displej.get()+"5");
 
     }
 
     public void btn66(ActionEvent actionEvent) {
-        if(displej.get()=="0") displej.set("");
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
         displej.set(displej.get()+"6");
     }
 
     public void btn99(ActionEvent actionEvent) {
-if(displej.get()=="0") displej.set("");
+if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
         displej.set(displej.get()+"9");
     }
 
 
     public void btn77(ActionEvent actionEvent) {
-        if(displej.get()=="0") displej.set("");
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
         displej.set(displej.get()+"7");
 
     }
 
     public void btn88(ActionEvent actionEvent) {
-        if(displej.get()=="0") displej.set("");
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
         displej.set(displej.get()+"8");
 
     }
 
     public void btn44(ActionEvent actionEvent) {
-        if(displej.get()=="0") displej.set("");
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
         displej.set(displej.get()+"4");
 
     }
 
-    public void minus(ActionEvent actionEvent) {
+    public void btn22(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"2");
+    }
+
+    public void btn11(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"1");
+    }
+    public void btn33(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"3");
+    }
+
+
+
+
+        public void minus(ActionEvent actionEvent) {
         prvi_operand.set(displej.get());
         displej.set("");
         operacija.set("-");
 
     }
 
+    public void plus(ActionEvent actionEvent) {
+        prvi_operand.set(displej.get());
+        displej.set("");
+        operacija.set("+");
+
+    }
+
+    public void mod(ActionEvent actionEvent) {
+        prvi_operand.set(displej.get());
+        displej.set("");
+        operacija.set("%");
+
+    }
+
+    public void dijeljenje(ActionEvent actionEvent) {
+        prvi_operand.set(displej.get());
+        displej.set("");
+        operacija.set("/");
+
+    }
+
+
     public void jednako(ActionEvent actionEvent) {
-        displej.set((Double.parseDouble(prvi_operand.get())-Double.parseDouble(displej.get()))+"");
+        switch(operacija.get()){
+            case "-":
+                displej.set((Double.parseDouble(prvi_operand.get())-Double.parseDouble(displej.get()))+"");
+                break;
+            case "+":
+                displej.set((Double.parseDouble(prvi_operand.get())+Double.parseDouble(displej.get()))+"");
+                break;
+            case "%":
+                displej.set((Double.parseDouble(prvi_operand.get())%Double.parseDouble(displej.get()))+"");
+                break;
+            case "*":
+                displej.set((Double.parseDouble(prvi_operand.get())*Double.parseDouble(displej.get()))+"");
+                break;
+            case "/":
+                displej.set((Double.parseDouble(prvi_operand.get())/Double.parseDouble(displej.get()))+"");
+                break;
+
+        }
+        operacija.set("=");
+
+
 
 
     }
 
     public void btn00(ActionEvent actionEvent) {
-        displej.set("0");
+
+        if(displej.get()!="0") displej.set(displej.get()+"0");
+
     }
 
     public void btnMnozenje(ActionEvent actionEvent) {
         prvi_operand.set(displej.get());
         displej.set("");
         operacija.set("*");
+    }
+
+
+    public void decimal(ActionEvent actionEvent) {
+        displej.set(displej.get()+".");
     }
 }
